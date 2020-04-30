38fe917c80689c5bc4e9b4b1eba94dc2760fb48e
yashar <yashar2004@gmail.com>
2020-05-01 01:25:48 +0430

    give atomics a try

--- countme/main.go
+++ countme/main.go
@@ -4,20 +4,21 @@ import (
 	"github.com/labstack/echo"
 	"io/ioutil"
 	"strconv"
+	"sync/atomic"
 )
 
-var numberOfReqs = 0
+var numberOfReqs = new(int64)
 
 func main() {
 	e := echo.New()
 	e.POST("/", func(c echo.Context) error {
 		body, _ := ioutil.ReadAll(c.Request().Body)
 		in, _ := strconv.Atoi(string(body))
-		numberOfReqs += in
+		atomic.AddInt64(numberOfReqs, int64(in))
 		return c.NoContent(200)
 	})
 	e.GET("/count", func(c echo.Context) error {
-		return c.String(200, strconv.Itoa(numberOfReqs))
+		return c.String(200, strconv.Itoa(int(atomic.LoadInt64(numberOfReqs))))
 	})
 
 	_ = e.Start(":80")
